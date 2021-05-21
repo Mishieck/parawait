0e61c55ee53fda782b9f3ad72e1f2a15e7e85de8
Mishieck <mishieckmwale@gmail.com>
2021-05-21 11:26:16 +0200

    tests(functionality): update tests with promises
    - Update tests with export promises
    - Do this to match the API

--- tests/functionality/functionality.js
+++ tests/functionality/functionality.js
@@ -1,13 +1,35 @@
-import io from "./io.js";
+import promises from "./promises.js";
 
 
-// Tests for io
-// io.nino();
-// io.niso();
-// io.nimo();
-// io.sino();
-// io.siso();
-// io.simo();
-// io.mino();
-// io.miso();
-// io.mimo();
+// Tests for promises
+// singleAction
+  // noInput
+  // promises.singleAction.noInput.noOutput();
+  // promises.singleAction.noInput.singleOutput();
+  // promises.singleAction.noInput.multipleOutputs();
+
+  // singleInput
+  // promises.singleAction.singleInput.noOutput();
+  // promises.singleAction.singleInput.singleOutput();
+  // promises.singleAction.singleInput.multipleOutputs();
+
+  // multipleInputs
+  // promises.singleAction.multipleInputs.noOutput();
+  // promises.singleAction.multipleInputs.singleOutput();
+  // promises.singleAction.multipleInputs.multipleOutputs();
+
+// multipleActions
+  // noInput
+  // promises.multipleActions.noInput.noOutput();
+  // promises.multipleActions.noInput.singleOutput();
+  // promises.multipleActions.noInput.multipleOutputs();
+
+  // singleInput
+  // promises.multipleActions.singleInput.noOutput();
+  // promises.multipleActions.singleInput.singleOutput();
+  // promises.multipleActions.singleInput.multipleOutputs();
+
+  // multipleInputs
+  // promises.multipleActions.multipleInputs.noOutput();
+  // promises.multipleActions.multipleInputs.singleOutput();
+  // promises.multipleActions.multipleInputs.multipleOutputs();
---
+++ tests/functionality/promises.js
@@ -0,0 +1,55 @@
+import { input, inputs, action, actions } from "./utils.js";
+import { promises } from "/dev/main.js";
+
+
+const test = async (input) => {
+	let start = performance.now();
+	const output = await promises(input);
+	const time = performance.now() - start;
+	start = performance.now();
+	console.log({ output, time });
+};
+
+const actionCount = 10;
+const SINGLE_ACTION_INPUT = { action, actionCount };
+const MULTIPLE_ACTIONS_INPUT = { actions };
+
+const singleAction = {
+  noInput: {
+    noOutput: () => test({ action, actionCount, outputType: "none" }),
+    singleOutput: () => test({ action, actionCount, outputType: "single" }),
+    multipleOutputs: () => test({ action, actionCount, outputType: "multiple" })
+  },
+  singleInput: {
+    noOutput: () => test({ action, actionCount, input, outputType: "none" }),
+    singleOutput: () => test({ action, actionCount, input, outputType: "single" }),
+    multipleOutputs: () => test({ action, actionCount, input, outputType: "multiple" })
+  },
+  multipleInputs: {
+    noOutput: () => test({ action, actionCount, inputs, outputType: "none" }),
+    singleOutput: () => test({ action, actionCount, inputs, outputType: "single" }),
+    multipleOutputs: () => test({ action, actionCount, inputs, outputType: "multiple" })
+  }
+}
+
+
+const multipleActions = {
+  noInput: {
+    noOutput: () => test({ actions, outputType: "none" }),
+    singleOutput: () => test({ actions, outputType: "single" }),
+    multipleOutputs: () => test({ actions, outputType: "multiple" })
+  },
+  singleInput: {
+    noOutput: () => test({ actions, input, outputType: "none" }),
+    singleOutput: () => test({ actions, input, outputType: "single" }),
+    multipleOutputs: () => test({ actions, input, outputType: "multiple" })
+  },
+  multipleInputs: {
+    noOutput: () => test({ actions, inputs, outputType: "none" }),
+    singleOutput: () => test({ actions, inputs, outputType: "single" }),
+    multipleOutputs: () => test({ actions, inputs, outputType: "multiple" })
+  }
+}
+
+
+export default { singleAction, multipleActions };
