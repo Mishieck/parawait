743eec77974a7a4358afaceb91749b04a2891c84
Mishieck <mishieckmwale@gmail.com>
2021-05-24 13:31:04 +0200

    refactor: reverse order of execution

--- src/perform-actions.ts
+++ src/perform-actions.ts
@@ -1,17 +1,20 @@
 import { Action, Input, Onerror, Output, PromisesOutput } from "./types.js";
 
-
 type PerformActionInput = {
   getAction: Function;
   actionCount: number;
   getInput: Function;
   setOutput: Function;
   getOutput: Function;
-	onerror: Onerror;
+  onerror: Onerror;
 };
 
 type PerformActions = (options: PerformActionInput) => PromisesOutput;
-type Promisify = (action: Action, input: Input, actionIndex: number) => PromisesOutput;
+type Promisify = (
+  action: Action,
+  input: Input,
+  actionIndex: number
+) => PromisesOutput;
 
 const performActions: PerformActions = (options) => {
   const {
@@ -20,29 +23,34 @@ const performActions: PerformActions = (options) => {
     getInput,
     setOutput,
     getOutput,
-    onerror = "throw"
+    onerror = "throw",
   } = options;
 
-	return new Promise((resolve: Function, reject: Function) => {
-		let resolveCount: number = actionCount;
-		
-		for (let actionIndex = actionCount; actionIndex--; ) {
-			promisify(getAction(actionIndex), getInput(actionIndex), actionIndex)
-			.then((output: any) => {
-				setOutput(output, actionIndex,);
-				--resolveCount || resolve(getOutput());
-			})
-      .catch((reason: any) => {
-        if (onerror === "throw") reject(new Error(`Action at ${actionIndex} was not performed successfully.`));
-        else if (onerror === "return") resolve();
-        else if (onerror === "break") resolve(getOutput());
-        else return;
-      });
-		};
-	});
+  return new Promise((resolve: Function, reject: Function) => {
+    let resolveCount: number = actionCount;
+
+    for (let actionIndex = 0; actionIndex < actionCount; actionIndex++) {
+      promisify(getAction(actionIndex), getInput(actionIndex), actionIndex)
+        .then((output: any) => {
+          setOutput(output, actionIndex);
+          --resolveCount || resolve(getOutput());
+        })
+        .catch((reason: any) => {
+          if (onerror === "throw")
+            reject(
+              new Error(
+                `Action at ${actionIndex} was not performed successfully.`
+              )
+            );
+          else if (onerror === "return") resolve();
+          else if (onerror === "break") resolve(getOutput());
+          else return;
+        });
+    }
+  });
 };
 
-const promisify: Promisify = async (action, input, actionIndex) => action(input, actionIndex);
-
+const promisify: Promisify = async (action, input, actionIndex) =>
+  action(input, actionIndex);
 
 export default performActions;
