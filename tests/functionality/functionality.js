c7f5ffde1507ef4aa5d8eda8188ea6c219e9f4c7
Mishieck <mishieckmwale@gmail.com>
2021-05-27 12:07:42 +0200

    sytle: format using prettier

--- tests/functionality/functionality.js
+++ tests/functionality/functionality.js
@@ -1,35 +1,34 @@
 import promises from "./promises.js";
 
-
 // Tests for promises
 // singleAction
-  // noInput
-  // promises.singleAction.noInput.noOutput();
-  // promises.singleAction.noInput.singleOutput();
-  // promises.singleAction.noInput.multipleOutputs();
+// noInput
+// promises.singleAction.noInput.noOutput();
+// promises.singleAction.noInput.singleOutput();
+// promises.singleAction.noInput.multipleOutputs();
 
-  // singleInput
-  // promises.singleAction.singleInput.noOutput();
-  // promises.singleAction.singleInput.singleOutput();
-  // promises.singleAction.singleInput.multipleOutputs();
+// singleInput
+// promises.singleAction.singleInput.noOutput();
+// promises.singleAction.singleInput.singleOutput();
+// promises.singleAction.singleInput.multipleOutputs();
 
-  // multipleInputs
-  // promises.singleAction.multipleInputs.noOutput();
-  // promises.singleAction.multipleInputs.singleOutput();
-  // promises.singleAction.multipleInputs.multipleOutputs();
+// multipleInputs
+// promises.singleAction.multipleInputs.noOutput();
+// promises.singleAction.multipleInputs.singleOutput();
+// promises.singleAction.multipleInputs.multipleOutputs();
 
 // multipleActions
-  // noInput
-  // promises.multipleActions.noInput.noOutput();
-  // promises.multipleActions.noInput.singleOutput();
-  // promises.multipleActions.noInput.multipleOutputs();
+// noInput
+// promises.multipleActions.noInput.noOutput();
+// promises.multipleActions.noInput.singleOutput();
+// promises.multipleActions.noInput.multipleOutputs();
 
-  // singleInput
-  // promises.multipleActions.singleInput.noOutput();
-  // promises.multipleActions.singleInput.singleOutput();
-  // promises.multipleActions.singleInput.multipleOutputs();
+// singleInput
+// promises.multipleActions.singleInput.noOutput();
+// promises.multipleActions.singleInput.singleOutput();
+// promises.multipleActions.singleInput.multipleOutputs();
 
-  // multipleInputs
-  // promises.multipleActions.multipleInputs.noOutput();
-  // promises.multipleActions.multipleInputs.singleOutput();
-  // promises.multipleActions.multipleInputs.multipleOutputs();
+// multipleInputs
+// promises.multipleActions.multipleInputs.noOutput();
+// promises.multipleActions.multipleInputs.singleOutput();
+// promises.multipleActions.multipleInputs.multipleOutputs();
